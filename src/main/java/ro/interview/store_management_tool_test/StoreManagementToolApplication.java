3412e90517e7c104354d08a1cbbe9ca8bf4328a2
mari <mariantoma95@gmail.com>
2025-05-09 18:42:12 +0300

    initial commit

---
+++ src/main/java/ro/interview/store_management_tool_test/StoreManagementToolApplication.java
@@ -0,0 +1,12 @@
+package ro.interview.store_management_tool_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementToolApplication.class, args);
+	}
+}
---
+++ src/test/java/ro/interview/store_management_tool_test/StoreManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.interview.store_management_tool_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
