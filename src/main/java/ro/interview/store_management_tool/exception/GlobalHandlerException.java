b5d2b78bd03581580ae2a2cd7ab3aab817726ec7
mari <mariantoma95@gmail.com>
2025-05-12 13:15:03 +0300

    handle validation error and added fallback exception handler

--- src/main/java/ro/interview/store_management_tool/exception/GlobalHandlerException.java
+++ src/main/java/ro/interview/store_management_tool/exception/GlobalHandlerException.java
@@ -1,6 +1,9 @@
 package ro.interview.store_management_tool.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ro.interview.store_management_tool.dto.ExceptionDto;
@@ -19,5 +22,23 @@ public class GlobalHandlerException {
     @ExceptionHandler(ProductAlreadyExistsException.class)
     public ResponseEntity<ExceptionDto> handleProductAlreadyExists(ProductAlreadyExistsException ex) {
         return ResponseEntity.status(CONFLICT).body(ExceptionDto.builder().message(ex.getMessage()).build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionDto> handleDeserializationError(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ExceptionDto.builder().message(ex.getMessage()).build());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionDto> handleDeserializationError(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ExceptionDto.builder().message(ex.getMessage()).build());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionDto> handleOtherExceptions(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ExceptionDto.builder().message("Unexpected error: " + ex.getMessage()).build());
     }
 }
